c9dbfab9f9ddc5c6ee9070ab2dad7dfdd7658913
Prashant saini <64362251+Prashantsaini25@users.noreply.github.com>
2021-05-04 21:05:58 +0530

    jenkins shell scripting file

--- jenkins.sh
+++ jenkins.sh
@@ -1,1 +1,16 @@
+sudo yum install wget -y
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo "https://pkg.jenkins.io/redhat-stable/jenkins.repo"
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+
+sudo yum upgrade -y
+
+sudo yum install jenkins java-1.8.0-openjdk-devel -y
+
+sudo systemctl daemon-reload
+
+sudo systemctl start jenkins
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
